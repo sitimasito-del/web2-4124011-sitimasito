403fb9c9e9b0c074875b8870113bb720c39cf1b7
JoenedSastra <joenedsastra812@gmail.com>
2026-04-18 09:42:09 +0700

    merubah harga

--- app/Http/Controllers/KatalogController.php
+++ app/Http/Controllers/KatalogController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class KatalogController extends Controller
 {
     private $produk = [
-        ["nama" => "Laptop ASUS", "harga" => 8500000],
+        ["nama" => "Laptop ASUS", "harga" => 9000000],
         ["nama" => "Mouse Wireless Logitech", "harga" => 200000],
         ["nama" => "Keyboard Mechanical", "harga" => 450000],
         ["nama" => "Monitor Samsung 24 inch", "harga" => 2500000],
